c94970889c5278b4124bb703ab3ef29fbe9a69d4
Egon Elbre <egonelbre@gmail.com>
2021-02-23 12:06:17 +0200

    go.mod: update for Go 1.16
    Also move CompileShared and CompileC to testsuite, this is the only
    package that needs them and is highly coupled with the code here.
    
    Change-Id: I107db4922079707327c5780d8aa688ef9cb0da63

--- testsuite/testdata_test.go
+++ testsuite/testdata_test.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"runtime"
 	"testing"
 	"time"
 
@@ -22,12 +23,12 @@ func TestC(t *testing.T) {
 	ctx := testcontext.NewWithTimeout(t, 5*time.Minute)
 	defer ctx.Cleanup()
 
-	libuplinkInclude := ctx.CompileShared(t, "uplink", "storj.io/uplink-c")
+	libuplinkInclude := CompileSharedAt(ctx, t, "../", "uplink", "storj.io/uplink-c")
 
 	currentdir, err := os.Getwd()
 	require.NoError(t, err)
 
-	definition := testcontext.Include{
+	definition := Include{
 		Header: filepath.Join(currentdir, "..", "uplink_definitions.h"),
 	}
 
@@ -41,14 +42,14 @@ func TestC(t *testing.T) {
 			t.Run(testName, func(t *testing.T) {
 				t.Parallel()
 
-				testexe := ctx.CompileC(t, testcontext.CompileCOptions{
+				testexe := CompileC(ctx, t, CompileCOptions{
 					Dest:    testName,
 					Sources: []string{ctest},
-					Includes: []testcontext.Include{
+					Includes: []Include{
 						libuplinkInclude,
 						definition,
-						testcontext.CLibMath,
-						testcontext.Include{
+						CLibMath,
+						Include{
 							Standard: true,
 							Library:  "pthread",
 						},
@@ -93,4 +94,108 @@ func TestC(t *testing.T) {
 			})
 		}
 	})
+}
+
+// CompileShared compiles pkg as c-shared.
+//
+// Note: cgo header paths are currently relative to package root.
+// TODO: support inclusion from other directories.
+func CompileSharedAt(ctx *testcontext.Context, t *testing.T, workDir, name string, pkg string) Include {
+	t.Helper()
+
+	if absDir, err := filepath.Abs(workDir); err == nil {
+		workDir = absDir
+	} else {
+		t.Fatal(err)
+	}
+
+	base := ctx.File("build", name)
+
+	args := []string{"build", "-buildmode", "c-shared"}
+	args = append(args, "-o", base+".so", pkg)
+
+	// not using race detector for c-shared
+	cmd := exec.Command("go", args...)
+	cmd.Dir = workDir
+
+	t.Log("exec:", cmd.Args)
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Error(string(out))
+		t.Fatal(err)
+	}
+	t.Log(string(out))
+
+	return Include{Header: base + ".h", Library: base + ".so"}
+}
+
+// CLibMath is the standard C math library (see `man math.h`).
+var CLibMath = Include{Standard: true, Library: "m"}
+
+// CompileCOptions stores options for compiling C source to an executable.
+type CompileCOptions struct {
+	Dest     string
+	Sources  []string
+	Includes []Include
+	NoWarn   bool
+}
+
+// CompileC compiles file as with gcc and adds the includes.
+func CompileC(ctx *testcontext.Context, t *testing.T, opts CompileCOptions) string {
+	t.Helper()
+
+	exe := ctx.File("build", opts.Dest+".exe")
+
+	var args = []string{}
+	if !opts.NoWarn {
+		args = append(args, "-Wall")
+		args = append(args, "-Wextra")
+		args = append(args, "-Wpedantic")
+		args = append(args, "-Werror")
+	}
+	args = append(args, "-ggdb")
+	args = append(args, "-o", exe)
+	for _, inc := range opts.Includes {
+		if inc.Header != "" {
+			args = append(args, "-I", filepath.Dir(inc.Header))
+		}
+		if inc.Library != "" {
+			if inc.Standard {
+				args = append(args,
+					"-l"+inc.Library,
+				)
+				continue
+			}
+			if runtime.GOOS == "windows" {
+				args = append(args,
+					"-L"+filepath.Dir(inc.Library),
+					"-l:"+filepath.Base(inc.Library),
+				)
+			} else {
+				args = append(args, inc.Library)
+			}
+		}
+	}
+	args = append(args, opts.Sources...)
+
+	/* #nosec G204 */ // This package is used for testing and the parameter's value are controlled by the above logic
+	cmd := exec.Command("gcc", args...)
+	t.Log("exec:", cmd.Args)
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Error(string(out))
+		t.Fatal(err)
+	}
+	t.Log(string(out))
+
+	return exe
+}
+
+// Include defines an includable library for gcc.
+type Include struct {
+	Header   string
+	Library  string
+	Standard bool
 }
